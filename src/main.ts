9a1c90c59522ea4e83b7086c3f223671e0426043
ing-brian-dev <ingbvn0@gmail.com>
2026-04-09 20:27:14 -0600

    Added validationPipe configuration to src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ // se necesita agregar npm i --save class-validator y class-transformer
+    whitelist: true //Esta configuracion filtra solo los nombres que esten en el DTO como un mapeo y expulsa los que no an sido declarados
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
