a2b599753614ed0813972e4832ac9612223ceffa
ing-brian-dev <ingbvn0@gmail.com>
2026-04-11 21:10:15 -0600

    Add: Configuration of environment variables in app.modules.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [CategoriesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    CategoriesModule
+  ],
   controllers: [AppController],
   providers: [AppService],//traerme funcionalidad de servicios
   // exports: [AppService]// Exportar funcionalidad a otros servicios
