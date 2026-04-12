9fa2560db2b1012665bce074701a4c3ab4579803
ing-brian-dev <ingbvn0@gmail.com>
2026-04-11 21:53:27 -0600

    Add: Generate category entity config

--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -1,1 +1,16 @@
-export class Category {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+
+//To use Active Record you must have to extends from BaseEntity
+//Data Maper is the Default
+@Entity()
+export class Category {
+
+    @PrimaryGeneratedColumn()
+    declare id: number;
+
+    @Column({
+        type: 'varchar',
+        length: 60
+    })
+    declare name: string;
+}
