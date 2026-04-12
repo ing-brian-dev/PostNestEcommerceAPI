6a145f5d10715f3c218030c79d2bb7b1176c2c09
ing-brian-dev <ingbvn0@gmail.com>
2026-04-11 21:59:08 -0600

    Create categories created

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -5,12 +5,12 @@ import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Controller('categories')
 export class CategoriesController {
-  constructor(private readonly categoriesService: CategoriesService) {}
+  constructor(
+    private readonly categoriesService: CategoriesService
+  ) {}
 
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
-    console.log(createCategoryDto);
-    
+  create(@Body() createCategoryDto: CreateCategoryDto) {    
     return this.categoriesService.create(createCategoryDto);
   }
 
