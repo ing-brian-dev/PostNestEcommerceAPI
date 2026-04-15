b2ee3cab3f23f2680fcacacceef6a692dedf7b15
ing-brian-dev <ingbvn0@gmail.com>
2026-04-14 21:34:32 -0600

    Injected Product and Category to product service and imported in product.module.ts

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { Product } from './entities/product.entity';
+import { Category } from '../categories/entities/category.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product,Category])],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
