9aa281ec36232894eb6f2e793b6e4b64a6ac801a
ing-brian-dev <ingbvn0@gmail.com>
2026-04-14 20:48:09 -0600

    Product entity created

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,41 @@
-export class Product {}
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Product {
+    @PrimaryGeneratedColumn()
+    declare id: number;
+
+    @Column({
+        type: 'varchar',
+        length: 60
+    })
+    declare name: string;
+
+    @Column({
+        type: 'varchar',
+        length: 120,
+        nullable: true,
+        default: 'default.sgv'
+    })
+    declare image: number;
+
+    @Column({
+        type: 'decimal'
+    })
+    declare price: number;
+
+    @Column({
+        type: 'int'
+    })
+    declare inventory: number;
+
+    @CreateDateColumn()
+    declare created: Date;
+
+    @UpdateDateColumn()
+    declare updated: Date;
+
+    // Add this column to your entity!
+    @DeleteDateColumn()
+    declare deletedAt?: Date;
+}
