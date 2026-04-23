b66cfe167a1b4921b70a4c0d5accc58a3234066c
ing-brian-dev <ingbvn0@gmail.com>
2026-04-23 15:49:35 -0600

    Created transaction.entity.ts

--- src/transactions/entities/transaction.entity.ts
+++ src/transactions/entities/transaction.entity.ts
@@ -1,1 +1,22 @@
-export class Transaction {}
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Transaction {
+    @PrimaryGeneratedColumn()
+    declare id: number;
+
+    @Column('decimal')
+    declare total: number;
+
+    
+
+    @CreateDateColumn()
+    declare createdAt: Date;
+
+    @UpdateDateColumn()
+    declare updatedAt: Date;
+
+    @DeleteDateColumn()
+    declare deletedAt?: Date;
+
+}
