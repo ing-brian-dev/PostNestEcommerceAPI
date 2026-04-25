f6d6b81cef3ad86aaa1fe6f80f0675e164824ae9
ing-brian-dev <ingbvn0@gmail.com>
2026-04-25 10:36:27 -0600

    Created coupon entity

--- src/coupons/entities/coupon.entity.ts
+++ src/coupons/entities/coupon.entity.ts
@@ -1,1 +1,26 @@
-export class Coupon {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Coupon {
+
+    @PrimaryGeneratedColumn()
+    declare id: number;
+
+    @Column({
+        type: 'varchar',
+        length: 30
+    })
+    declare name: string;
+
+
+    @Column({
+        type: 'integer'
+    })
+    declare percentage: number;
+
+
+    @Column({
+        type: 'date'
+    })
+    declare expirationDate: Date;
+}
