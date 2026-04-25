99228197286963f3ae7980e5a916d62307ec2b22
ing-brian-dev <ingbvn0@gmail.com>
2026-04-25 10:50:15 -0600

    Created create coupon service

--- src/coupons/coupons.module.ts
+++ src/coupons/coupons.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CouponsService } from './coupons.service';
 import { CouponsController } from './coupons.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Coupon } from './entities/coupon.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Coupon])],
   controllers: [CouponsController],
   providers: [CouponsService],
 })
