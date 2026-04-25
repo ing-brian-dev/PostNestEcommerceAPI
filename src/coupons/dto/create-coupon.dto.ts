dfb6def46a54cf4c0e23bbaa7d83d0f00791c06c
ing-brian-dev <ingbvn0@gmail.com>
2026-04-25 10:46:31 -0600

    Created coupon Dto

--- src/coupons/dto/create-coupon.dto.ts
+++ src/coupons/dto/create-coupon.dto.ts
@@ -1,1 +1,31 @@
-export class CreateCouponDto {}
+import { IsDateString, IsInt, IsNotEmpty, Max, Min } from "class-validator";
+
+export class CreateCouponDto {
+
+    @IsNotEmpty({
+        message: 'El nombre es requerido.'
+    })
+    declare name: string;
+
+    @IsNotEmpty({
+        message: 'El porcentaje es requerido.'
+    })
+    @IsInt({
+        message: 'El descuento debe der entre 1 y 100.'
+    })
+    @Max(100, {
+        message: 'El descuento maximo es de 100.'
+    })
+    @Min(1, {
+        message: 'El descuento minimo es de 1.'
+    })
+    declare percentage: number;
+
+    @IsNotEmpty({
+        message: 'La fecha es requerida.'
+    })
+    @IsDateString({}, {
+        message: 'Fecha no válida.'
+    })
+    declare expirationDate: Date;
+}
