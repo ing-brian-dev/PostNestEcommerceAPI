7cd6a2ed099aff36afd8d849c8232bd97000a124
ing-brian-dev <ingbvn0@gmail.com>
2026-04-25 13:06:45 -0600

    Implement seeder functionality with SeederModule and SeederService; add categories and products data files

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ // se necesita agregar npm i --save class-validator y class-transformer
-    whitelist: true //Esta configuracion filtra solo los nombres que esten en el DTO como un mapeo y expulsa los que no an sido declarados
+  app.useGlobalPipes(new ValidationPipe({ // You need to add `npm i --save class-validator` and `class-transformer`
+    whitelist: true // This configuration filters only the names that are in the DTO as a mapping and excludes those that have not been declared.
   }));
   await app.listen(process.env.PORT ?? 3000);
 }
---
+++ src/seeder/seeder.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class SeederService {
+
+    async seed() {
+        console.log('Desde seeder');
+    }
+}
